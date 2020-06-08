901ddab64da74b93beefdfc8773a9423656c1eed
Pierrette Mugisha <mupierrix@yahoo.fr>
2020-06-07 21:34:49 -0300

    Code in C#

---
+++ Katas/CSharp/CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+
+        static int fib(int n)
+        {
+            return n < 2 ? n : fib(n - 1) + fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.fib(8));
+        }
+    }
+}
